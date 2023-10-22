aa443e741f5d1f12ac1fb3003e9fa38510c81d01
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-22 09:02:48 +0200

    html for header done

--- JokeGenerator/src/components/Header.jsx
+++ JokeGenerator/src/components/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Favorites from '../views/Favorites'
+import Alljokes from '../views/Alljokes'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <section>
+            <article>
+                <h1>Joke Generator</h1>
+                <img src="src/assets/Group (1).png" alt="logoIcon" />
+            </article>
+        </section>
+        <section>
+            <nav>
+                <ul>
+                    <li>
+                        <button>All Jokes</button>
+                    </li>
+                    <li>
+                        <button>Favorites</button>
+                    </li>
+                    <li>
+                        <img src="src/assets/Vector (6).png" alt="sun" />
+                        <img src="src/assets/Vector (5).png" alt="moon" />
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    </div>
   )
 }
 
