f5c2cc4dfa47f45f53a450cb48dda9aa85b60561
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-24 11:31:21 +0200

    done with js, lightMode button was challenging

--- JokeGenerator/src/components/Header.jsx
+++ JokeGenerator/src/components/Header.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
-import './Header.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Header.css';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Header() {
+    const navigate = useNavigate();
+    const [toggleMode, setToggleMode] = useState(false);
+    const [animationClass, setAnimationClass] = useState('');
 
-    const navigate = useNavigate()
+    function handleClick() {
+        setToggleMode(prev => !prev);
+        setAnimationClass(toggleMode ? 'slide-out' : 'slide-in');
 
-  return (
-    <div className='header'>
-        <section className='logoSec'>
-            <article>
-                <h1>JOKE GENERATOR</h1>
-                <img src="src/assets/Group (1).png" alt="logoIcon" />
-            </article>
-        </section>
-        <section className='navSec'>
-            <nav>
-                <ul>
-                    <li>
-                        <button onClick={() => navigate("/alljokes")}>All Jokes</button>
-                    </li>
-                    <li>
-                        <button onClick={() => navigate("/favorites")}>Favorites</button>
-                    </li>
-                    <li className='lightMode'>
-                        <img src="src/assets/Vector (6).png" alt="sun" />
-                        <div className='circle'></div>
-                        <img src="src/assets/Vector (5).png" alt="moon" />
-                    </li>
-                </ul>
-            </nav>
-        </section>
-    </div>
-  )
+        document.body.classList.toggle('dark-mode', !toggleMode);
+        document.body.classList.toggle('switch', !toggleMode);
+    }
+
+    return (
+        <div className='header'>
+            <section className='logoSec'>
+                <article onClick={() => navigate("/")}>
+                    <h1>JOKE GENERATOR</h1>
+                    <img src="src/assets/Group (1).png" alt="logoIcon" />
+                </article>
+            </section>
+            <section className='navSec'>
+                <nav>
+                    <ul>
+                        <li>
+                            <button onClick={() => navigate("/alljokes")}>All Jokes</button>
+                        </li>
+                        <li>
+                            <button onClick={() => navigate("/favorites")}>Favorites</button>
+                        </li>
+                        <li className='lightMode' onClick={handleClick}>
+                            <img src="src/assets/Vector (6).png" alt="sun"/>
+                            <div className={`circle ${animationClass}`}></div>
+                            <img src="src/assets/Vector (5).png" alt="moon"/>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    )
 }
 
-export default Header
+export default Header;
