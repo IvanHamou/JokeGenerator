73d657e11685ba07d231571d0445a84853d87e81
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-27 10:03:59 +0200

    working on filter component

--- JokeGenerator/src/components/Filter.jsx
+++ JokeGenerator/src/components/Filter.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import "./Filter.css"
 
 function Filter() {
   return (
-    <div>Filter</div>
+    <div className='filterPage'>
+      <section className='filterTitle'>
+        <h2>Filter</h2>
+      </section>
+      <section className='filterOptions'>
+        <article>
+          <h4>Select Category</h4>
+          <select>
+            <option value="0">Programming</option>
+            <option value="1">Misc</option>
+            <option value="2">Dark</option>
+            <option value="3">Pun</option>
+            <option value="4">Spooky</option>
+            <option value="5">Christmas</option>
+          </select>
+        </article>
+        <article>
+          <h4>Select Language</h4>
+          <select>
+            <option value="0">Czech</option>
+            <option value="1">German</option>
+            <option value="2">English</option>
+            <option value="3">Spanish</option>
+            <option value="4">French</option>
+            <option value="5">Portuguese</option>
+          </select>
+        </article>
+        <article>
+          <h4>Includes Word</h4>
+          <input type="text" />
+        </article>
+      </section>
+    </div>
   )
 }
 
