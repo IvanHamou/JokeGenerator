2e1f15006d09929e78533552ad2f099b2f9371c8
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-29 08:39:51 +0100

    Nav on smaller devices

--- JokeGenerator/src/components/Header.jsx
+++ JokeGenerator/src/components/Header.jsx
@@ -5,30 +5,41 @@ import { useNavigate } from 'react-router-dom';
 function Header() {
     const navigate = useNavigate();
     const [toggleMode, setToggleMode] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
     const [animationClass, setAnimationClass] = useState('');
+    const [menuDisplay, setMenuDisplay] = useState('');
 
-    function handleClick() {
-        setToggleMode(prev => !prev);
-        setAnimationClass(toggleMode ? 'slide-out' : 'slide-in');
+    function switchMode() {
+        setToggleMode(prev => !prev)
+        setAnimationClass(toggleMode ? 'slide-out' : 'slide-in')
 
-        document.body.classList.toggle('dark-mode', !toggleMode);
-        document.body.classList.toggle('switch', !toggleMode);
+        document.body.classList.toggle('dark-mode', !toggleMode)
+        document.body.classList.toggle('switch', !toggleMode)
+    }
+
+    function openMenu() {
+        setToggleMenu(prev => !prev)
+        setMenuDisplay(!toggleMenu ? 'visible' : '')
+
+        //document.body.classList.toggle('nav', !toggleMode)
     }
 
     return (
         <div className='header'>
-            <section className='logoSec'>
-                <article onClick={() => navigate("/")}>
-                    <h1>JOKE GENERATOR</h1>
-                    <img src="src/assets/Group (1).png" alt="logoIcon" />
+            <div>
+                <section className='logoSec'>
+                    <article onClick={() => navigate("/")}>
+                        <h1>JOKE GENERATOR</h1>
+                        <img src="src/assets/Group (1).png" alt="logoIcon" />
+                    </article>
+                </section>
+                    <article className='hamburger' onClick={openMenu}>
+                        <div className='hamburgerLine'></div>
+                        <div className='hamburgerLine'></div>
+                        <div className='hamburgerLine'></div>
                 </article>
-            </section>
-            <section className='navSec'>
-                <article className='hamburger'>
-                    <div className='hamburgerLine'></div>
-                    <div className='hamburgerLine'></div>
-                    <div className='hamburgerLine'></div>
-                </article>
+            </div>
+            <section className={`navSec ${menuDisplay}`}>
                 <nav>
                     <ul>
                         <li>
@@ -37,7 +48,7 @@ function Header() {
                         <li>
                             <button onClick={() => navigate("/favorites")}>Favorites</button>
                         </li>
-                        <li className='lightMode' onClick={handleClick}>
+                        <li className='lightMode' onClick={switchMode}>
                             <img src="src/assets/Vector (6).png" alt="sun"/>
                             <div className={`circle ${animationClass}`}></div>
                             <img src="src/assets/Vector (5).png" alt="moon"/>
